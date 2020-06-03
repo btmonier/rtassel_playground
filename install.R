c79d85b5c7b90798fcdac269558a6fcbced6a605
Brandon <brandon.monier@gmail.com>
2020-06-03 14:11:20 -0400

    Initial commit

---
+++ install.R
@@ -0,0 +1,13 @@
+## Install MRAN packages ----
+install.packages("rJava")
+install.packages("tidyverse")
+install.packages("devtools")
+install.packages("BiocManager")
+
+
+## Install Bioconductor packages ----
+BiocManager::install("SummarizedExperiment")
+
+
+## Install rTASSEL ----
+devtools::install_bitbucket("bucklerlab/rTASSEL", ref = "rTAS-006-genome-filtration")
